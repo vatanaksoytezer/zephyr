a56a7450fd19cc2f32b95ebb66992f6d44c6945f
VatanTezer <vatanaksoytezer@gmail.com>
2018-04-03 07:05:44 +0300

    Add Sophus

---
+++ Sophus/build/SophusConfig.cmake
@@ -0,0 +1,17 @@
+################################################################################
+# Sophus source dir
+set( Sophus_SOURCE_DIR "/home/tezerv/thesis_ws/src/Sophus")
+
+################################################################################
+# Sophus build dir
+set( Sophus_DIR "/home/tezerv/thesis_ws/src/Sophus/build")
+
+################################################################################
+set( Sophus_INCLUDE_DIR  "/home/tezerv/thesis_ws/src/Sophus;/usr/include/eigen3" )
+set( Sophus_INCLUDE_DIRS  "/home/tezerv/thesis_ws/src/Sophus;/usr/include/eigen3" )
+
+set( Sophus_LIBRARIES    "/home/tezerv/thesis_ws/src/Sophus/build/libSophus.so" )
+set( Sophus_LIBRARY      "/home/tezerv/thesis_ws/src/Sophus/build/libSophus.so" )
+
+set( Sophus_LIBRARY_DIR  "/home/tezerv/thesis_ws/src/Sophus/build" )
+set( Sophus_LIBRARY_DIRS "/home/tezerv/thesis_ws/src/Sophus/build" )
